a4648c02de701a5af9677b1f8e9693f8923847ae
MrSkl1f <sklifden@mail.ru>
2020-10-18 00:51:33 +0300

    Add Task 5.1

---
+++ Task5/Task1/index.js
@@ -0,0 +1,68 @@
+"use strict";
+
+// импортируем необходимые библиотеки
+const express = require("express");
+const fs = require("fs");
+
+// запускаем сервер
+const app = express();
+const port = 5000;
+app.listen(port);
+console.log(`Server on port ${port}`);
+
+// отправка статических файлов
+const way = __dirname + "/static";
+app.use(express.static(way));
+
+// заголовки в ответ клиенту
+app.use(function(req, res, next) {
+    res.header("Cache-Control", "no-cache, no-store, must-revalidate");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Origin", "*");
+    next();
+});
+
+function loadBody(request, callback) {
+    let body = [];
+    request.on('data', (chunk) => {
+        body.push(chunk);
+    }).on('end', () => {
+        body = Buffer.concat(body).toString();
+        callback(body);
+    });
+}
+[{"mail" : "asf", "name" : "asf", "number" : "1241241"}]
+function checkUnique(a, b, c) {
+    let info = JSON.parse(fs.readFileSync("file.txt", "utf-8"));
+    let check = true;
+    for (let i = 0; i < info.length && check; i++) {
+        let obj = info[i];
+        if (obj["mail"] == a || obj["number"] == c) {
+            check = false;
+        }
+    }
+    return check;
+}
+
+app.post("/save/info", function(request, response) {
+    loadBody(request, function(body) {
+        const obj = JSON.parse(body);
+        const mail = obj["mainInfo"];
+        const name = obj["nameInfo"];
+        const number = obj["numberInfo"];
+        if (checkUnique(mail, name, number)) {
+            let info = JSON.parse(fs.readFileSync("file.txt", "utf-8"));
+            info.push({"mail" : mail, "name" : name, "number" : number});
+            const obj = JSON.stringify(info);
+            fs.writeFileSync("file.txt", obj);
+            response.end(JSON.stringify({
+                result: "Save content ok"
+            }));
+        }
+        else {
+            response.end(JSON.stringify({
+                result: "Not unique mail or number"
+            }));
+        }
+    });
+});
